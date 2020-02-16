9937f63c604a7ee9f522ae0a8dc223872fad7a6d
ohno <ohno@gr1.jp>
2020-02-16 17:17:37 +0900

    config.js 修正

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,9 +15,9 @@ module.exports = {
       '/about/',
       '/blog/',
     ],
-    head: [
-      ['script', { src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js' }],
-      ['script', { src: '/js/jquery.bxslider.js' }]
-    ]
-  }
+  },
+  head: [
+    ['script', { src: 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js' }],
+    ['script', { src: '/js/jquery.bxslider.js' }]
+  ]
 };
